2df2af3577ba0c8f4b0c9a9644ce5dbaeb50df7a
carsonbring <carsonbring@gmail.com>
2023-12-13 14:25:14 -0600

    day 1 progress

--- src/Day1.java
+++ src/Day1.java
@@ -1,17 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Day1 {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String filepath = "../input/InputDay1";
+        try(BufferedReader reader = new BufferedReader(new FileReader(filepath))){
+            String line;
+            int sum;
+            String regex= "\\d+";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher;
+            while((line = reader.readLine()) != null){
+                    number
+                   matcher = pattern.matcher(line);
+                   while(matcher.find()){
+                       String match = matcher.group();
+
+
+
+                   }
+            }
         }
+
     }
 }
