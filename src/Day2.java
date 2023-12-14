26ec853fab9fedf847af93f76219ca7253071b0c
carsonbring <carsonbring@gmail.com>
2023-12-13 20:45:33 -0600

    day 2 part 1

---
+++ src/Day2.java
@@ -0,0 +1,70 @@
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day2 {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        System.out.println("Part 1:");
+        part1();
+        System.out.println("Part 2:");
+
+    }
+
+    public static void part1() throws FileNotFoundException, IOException {
+
+        ArrayList<Bag_Day2> bagList = new ArrayList<>();
+        String filepath = "./input/InputDay2";
+        String regex = "Game (\\d+):((?:\\s*(\\d+)\\s*([a-zA-Z]+),?;?)+)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher;
+        String line;
+        int sum = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+            while((line = reader.readLine()) != null){
+                matcher = pattern.matcher(line);
+                while( matcher.find()){
+                    Boolean flag = true;
+                    int id = Integer.parseInt(matcher.group(1));
+                    String group2 = matcher.group(2);
+                    Pattern valuesPattern = Pattern.compile("(\\d+)\\s*([a-zA-Z]+)");
+                    Matcher valuesMatcher = valuesPattern.matcher(group2);
+
+                    while(valuesMatcher.find()){
+                        int quantity = Integer.parseInt(valuesMatcher.group(1));
+                        String color = valuesMatcher.group(2);
+                        switch (color){
+                            case "red":
+                                if(quantity > 12 ){
+                                    flag = false;
+                                }
+                                break;
+                            case "blue":
+                                if(quantity > 14)
+                                    flag = false;
+                                break;
+                            case "green":
+                                if(quantity > 13)
+                                    flag = false;
+                                break;
+
+                        }
+
+                    }
+                    if(flag){
+                        sum = sum + id;
+                    }
+
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+    public static void part2(){
+
+    }
+}
