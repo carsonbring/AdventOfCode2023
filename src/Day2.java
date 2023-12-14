e4e8828cc601ed6a4162900d40fcad9fe9262318
carsonbring <carsonbring@gmail.com>
2023-12-13 21:00:42 -0600

    day 2 done

--- src/Day2.java
+++ src/Day2.java
@@ -12,7 +12,7 @@ public class Day2 {
         System.out.println("Part 1:");
         part1();
         System.out.println("Part 2:");
-
+        part2();
     }
 
     public static void part1() throws FileNotFoundException, IOException {
@@ -64,7 +64,54 @@ public class Day2 {
         }
         System.out.println(sum);
     }
-    public static void part2(){
+    public static void part2() throws FileNotFoundException, IOException{
+        ArrayList<Bag_Day2> bagList = new ArrayList<>();
+        String filepath = "./input/InputDay2";
+        String regex = "Game (\\d+):((?:\\s*(\\d+)\\s*([a-zA-Z]+),?;?)+)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher;
+        String line;
+        long sum = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+            while((line = reader.readLine()) != null){
+                matcher = pattern.matcher(line);
+                while( matcher.find()){
+                    int maxred = 0;
+                    int maxblue = 0;
+                    int maxgreen = 0;
+                    int id = Integer.parseInt(matcher.group(1));
+                    String group2 = matcher.group(2);
+                    Pattern valuesPattern = Pattern.compile("(\\d+)\\s*([a-zA-Z]+)");
+                    Matcher valuesMatcher = valuesPattern.matcher(group2);
 
+                    while(valuesMatcher.find()){
+                        int quantity = Integer.parseInt(valuesMatcher.group(1));
+                        String color = valuesMatcher.group(2);
+                        switch (color){
+                            case "red":
+                                if(quantity > maxred){
+                                    maxred = quantity;
+                                }
+                                break;
+                            case "blue":
+                                if(quantity > maxblue)
+                                    maxblue = quantity;
+                                break;
+                            case "green":
+                                if(quantity > maxgreen){
+                                    maxgreen = quantity;
+                                }
+                                break;
+
+                        }
+
+                    }
+                    long power = (long) maxred * maxgreen * maxblue;
+                    sum = sum + power;
+
+                }
+            }
+        }
+        System.out.println(sum);
     }
 }
