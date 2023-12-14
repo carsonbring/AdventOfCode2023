efb73d1121f916b2e88e53fa1e398a3d4c621b56
carsonbring <carsonbring@gmail.com>
2023-12-14 14:38:04 -0600

    Day 3 part 1 complete

--- src/Day3.java
+++ src/Day3.java
@@ -26,9 +26,55 @@ public class Day3 {
 
     }
     public static void part1(char[][] engine) throws FileNotFoundException, IOException{
-
+        boolean[][] visited = new boolean[engine.length][engine[0].length];
+        int sum = 0;
+        for(int i = 0; i < engine.length; i++){
+            for(int j = 0; j < engine.length; j++){
+                if(Character.isDigit(engine[i][j])){
+                    if(isSpecialAdjacent(engine, i, j)){
+                        try {
+                            String capturedNum = captureNumber(engine, visited, i, j);
+                            sum = sum + Integer.parseInt(capturedNum);
+                        }catch(NumberFormatException e){}
+                    }
+                }
+            }
+        }
+        System.out.println(sum);
     }
     public static void part2(char[][] engine) throws FileNotFoundException, IOException{
 
+    }
+    private static boolean isSpecialCharacter(char ch) {
+        String specialCharacters = "!@#$%^&*()-=_+[]{}|;:'\\,<>?/";
+        return specialCharacters.contains(String.valueOf(ch));
+    }
+
+    private static String captureNumber(char[][] engine, boolean[][] visited, int i, int j) {
+        //Checking to see if the current cell is out of bounds, already visited, or not a digit
+        if (i < 0 || i >= engine.length || j < 0 || j >= engine[0].length || visited[i][j] || !Character.isDigit(engine[i][j])) {
+            return "";
+        }
+        visited[i][j] = true;
+        char currentDigit = engine[i][j];
+        return captureNumber(engine, visited, i, j - 1) +
+                currentDigit +
+                captureNumber(engine, visited, i, j + 1);
+    }
+
+    private static boolean isSpecialAdjacent(char[][] engine, int i, int j) {
+        int rows = engine.length;
+        int cols = engine[0].length;
+        //for loop that checks for boundaries by using min and max functions
+        for (int x = Math.max(0, i - 1); x <= Math.min(i + 1, rows - 1); x++) {
+            for (int y = Math.max(0, j - 1); y <= Math.min(j + 1, cols - 1); y++) {
+                if (x != i || y != j) {
+                    if (isSpecialCharacter(engine[x][y])) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
