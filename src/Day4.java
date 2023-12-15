b07059cfb7a1a925e680e4d4b52dc798d6d104e1
carsonbring <carsonbring@gmail.com>
2023-12-15 13:37:58 -0600

    Day 4 part 2 complete

--- src/Day4.java
+++ src/Day4.java
@@ -3,6 +3,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class Day4 {
@@ -57,6 +59,60 @@ public class Day4 {
         }
     }
     public static void part2() throws FileNotFoundException, IOException{
+        Map<Integer, Integer> mapOfCards = new HashMap<>();
+        for( int i = 1; i < 214; i++){
+            mapOfCards.put(i, 1);
+        }
+        String filepath = "./input/InputDay4";
+        String regex = "\\b(\\d+)\\b";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher;
+        String line;
+        int sum = 0;
+        int cardNum = 1;
+        try(BufferedReader reader = new BufferedReader(new FileReader(filepath))){
+            while((line = reader.readLine()) != null){
+                ArrayList<Integer> winningNums = new ArrayList<Integer>();
+                ArrayList<Integer> elfNums = new ArrayList<Integer>();
+                matcher = pattern.matcher(line);
+                int points = 0;
+                int counter = 0;
+                while(matcher.find()){
+                    if(counter == 0){
+                        counter = counter + 1;
+                        continue;
+                    }
+                    String match = matcher.group(1);
+                    if(counter < 11){
+                        winningNums.add(Integer.parseInt(match));
+                    }else{
+                        elfNums.add(Integer.parseInt(match));
+                    }
+                    counter = counter + 1;
+                }
+
+                for(Integer num : winningNums){
+                    if(elfNums.contains(num)){
+                        points++;
+                    }
+                }
+                Integer multiplier = mapOfCards.get(cardNum);
+                int tempCardNum = cardNum + 1;
+                while(points > 0){
+                    for(int i = 0; i < multiplier; i++) {
+                        Integer updatedValue = mapOfCards.get(tempCardNum) + 1;
+                        mapOfCards.put(tempCardNum, updatedValue);
+                    }
+                    tempCardNum++;
+                    points--;
+                }
+                cardNum++;
+            }
+            for( Integer value : mapOfCards.values() ){
+                sum += value;
+            }
+            System.out.println(sum);
+        }
     }
 
 }
